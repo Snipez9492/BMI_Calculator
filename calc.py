8f7ed9f7a5fa58a88ad10bdacf495c05260116ab
Snipez9492 <waleibk92@gmail.com>
2021-06-22 21:36:38 -0400

    Second Chance

--- calc.py
+++ calc.py
@@ -1,1 +1,18 @@
-print("I hope this works.")
+import re
+
+pattern = "[0-9]+"
+
+name = input("What is your name: ")
+
+if re.search(pattern, name):
+    print("Okay great")
+else:
+    print("Invalid input. Please try again.")
+
+pattern2 = "[a-zA-Z]"
+try:
+    question = input("What is your weight in lbs: ")
+    if re.search(pattern2, question):
+        raise ValueError("Invalid entry.")
+except ValueError as v:
+    print(v)
