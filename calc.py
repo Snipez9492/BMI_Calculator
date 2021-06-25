7810ddd7efe11407870178a47616df02df0da520
Snipez9492 <waleibk92@gmail.com>
2021-06-25 08:17:49 -0400

    Final commit

--- calc.py
+++ calc.py
@@ -8,18 +8,55 @@ while True:
         print("Welcome", name)
         break
     else:
-        print("WARNING: Only letters, no numbers or special characters. You have", attempts, "left.")
         attempts -= 1
+        print("WARNING: Only letters, no numbers or special characters. You have", attempts, "left.")
         if attempts == 0:
             print("You have exceeded the amount of attempts. Goodbye!")
             break
         continue
 
-
-pattern2 = "[a-zA-Z]"
+pattern = "[a-zA-Z]"
 try:
-    question = input("What is your weight in lbs: ")
-    if re.search(pattern2, question):
+    weight = input("What is your weight in lbs: ")
+    if re.search(pattern, weight):
         raise ValueError("Invalid entry.")
 except ValueError as v:
     print(v)
+
+height = input("What is you height: ")
+
+lst = []
+for x in height:
+    if x.isdigit():
+        x = int(x)
+        lst.append(x)
+    if len(lst) == 2:
+        break
+
+feet = lst[0] * 12
+inches = lst[1] + 0
+
+real_height = feet + inches
+print(real_height, 'inches in height')
+
+'''
+BMI Formula is (weight[lbs]/height[lbs]/height[lbs]) * 703
+'''
+
+
+def calc_BMI():
+    BMI = (int(weight) / real_height / real_height) * 703
+    real_bmi = round(BMI, 1)
+    return real_bmi
+
+
+print(name, "your BMI is", calc_BMI())
+
+if calc_BMI() < 18.5:
+    print("You are underweight.")
+elif 18.5 < calc_BMI() <= 24.9:
+    print("You have normal weight")
+elif 24.9 < calc_BMI() <= 29.9:
+    print("You are overweight")
+elif calc_BMI() > 30.0:
+    print("You are overweight and need help.")
